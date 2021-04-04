ac0b0961ea98a324ae7a52a5d9311a1a39b87b6b
mjiam <mjiam@student.codam.nl>
2021-04-04 20:10:32 +0200

    Updated description of lstadd_front

--- src/lst/ft_lstadd_front.c
+++ src/lst/ft_lstadd_front.c
@@ -6,14 +6,14 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/07 17:36:06 by mjiam         #+#    #+#                 */
-/*   Updated: 2021/03/23 17:10:53 by mjiam         ########   odam.nl         */
+/*   Updated: 2021/04/04 20:09:42 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftx.h"
 
 /* Adds the element new at the beginning of the list,
-alst being the first link of the list. */
+head being the first link of the list. */
 
 void	ft_lstadd_front(t_list **head, t_list *new)
 {
