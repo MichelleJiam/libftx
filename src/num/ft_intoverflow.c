7025e1f04eeff23a28632db8d567127c6756e5c2
mjiam <mjiam@student.codam.nl>
2021-04-09 21:34:34 +0200

    Added ft_intoverflow

---
+++ src/num/ft_intoverflow.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_intoverflow.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/04/09 21:21:10 by mjiam         #+#    #+#                 */
+/*   Updated: 2021/04/09 21:32:20 by mjiam         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftx.h"
+
+/* Checks for integer overflow in sum of a and b. 
+If overflow, teturns 1 without modifying result. 
+If not, updates result with sum and returns 0. */
+
+int	ft_intoverflow(int *result, int a, int b)
+{
+	if (a > INT_MAX - b)
+		return (1);
+	else
+	{
+		*result = a + b;
+		return (0);
+	}
+}
