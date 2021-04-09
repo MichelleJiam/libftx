df38c6c240094cd3f089604258a2be4763f7c7aa
mjiam <mjiam@student.codam.nl>
2021-04-09 23:05:42 +0200

    Add ft_strictatoi and fixed ft_intoverflow

--- src/num/ft_intoverflow.c
+++ src/num/ft_intoverflow.c
@@ -6,19 +6,21 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/09 21:21:10 by mjiam         #+#    #+#                 */
-/*   Updated: 2021/04/09 21:32:20 by mjiam         ########   odam.nl         */
+/*   Updated: 2021/04/09 22:39:54 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftx.h"
 
 /* Checks for integer overflow in sum of a and b. 
-If overflow, teturns 1 without modifying result. 
+If overflow, returns 1 without modifying result. 
 If not, updates result with sum and returns 0. */
 
 int	ft_intoverflow(int *result, int a, int b)
 {
-	if (a > INT_MAX - b)
+	if (a > 0 && b > 0 && a > INT_MAX - b)
+		return (1);
+	else if (a < 0 && b < 0 && a < INT_MIN - b)
 		return (1);
 	else
 	{
---
+++ src/num/ft_strictatoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strictatoi.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/10/29 17:54:49 by mjiam         #+#    #+#                 */
+/*   Updated: 2021/04/09 23:03:53 by mjiam         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftx.h"
+
+/* Converts str to int representation and saves in *num. Returns 0 if success.
+If over or under int limits, returns -1 and doesn't change *num. */
+
+int	ft_strictatoi(const char *str, int *num)
+{
+	int			negative;
+	intmax_t	n;
+
+	n = 0;
+	ft_skipws(&str, 0);
+	if (!str)
+		return (0);
+	negative = (*str == '-');
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str && ft_isdigit(*str))
+	{
+		n = n * 10 + (*str - '0');
+		if ((!negative && n > INT_MAX) || (negative && ((n * -1) < INT_MIN)))
+			return (-1);
+		str++;
+	}
+	if (negative)
+		*num = n * -1;
+	else
+		*num = n;
+	return (0);
+}
