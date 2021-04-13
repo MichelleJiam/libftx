b485e8f2400b51b0f1218592003624938dcd0f46
mjiam <mjiam@student.codam.nl>
2021-04-13 23:36:00 +0200

    Added ft_dlstclear

--- inc/libftx.h
+++ inc/libftx.h
@@ -6,7 +6,7 @@
 /*   By: mjiam <mjiam@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/30 16:36:42 by mjiam         #+#    #+#                 */
-/*   Updated: 2021/04/13 23:12:05 by mjiam         ########   odam.nl         */
+/*   Updated: 2021/04/13 23:32:57 by mjiam         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ void	ft_putstr_fd(char *s, int fd);
 t_dlist	*ft_dlst_mergesort(t_dlist *lst);
 void	ft_dlstadd_back(t_dlist **head, t_dlist *new);
 void	ft_dlstadd_front(t_dlist **head, t_dlist *new);
+void	ft_dlstclear(t_dlist **lst, void (*del)(void *));
 void	ft_dlstdelnode(t_dlist **head, t_dlist *node, void (*del)(void *));
 t_dlist	*ft_dlstnew(void *content, t_dlist *previous);
 void	ft_lstadd_back(t_list **head, t_list *new);
