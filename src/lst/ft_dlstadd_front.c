ad049e77cc260a2cb8e70cf4a2b8a70f09520995
mjiam <mjiam@student.codam.nl>
2021-04-18 01:24:53 +0200

    Brought dlstadd_front description in line with prototype

--- src/lst/ft_dlstadd_front.c
+++ src/lst/ft_dlstadd_front.c
@@ -13,7 +13,7 @@
 #include "libftx.h"
 
 /* Adds the element new at the beginning of the double-ended list,
-alst being the first link of the list. */
+head being the first link of the list. */
 
 void	ft_dlstadd_front(t_dlist **head, t_dlist *new)
 {
